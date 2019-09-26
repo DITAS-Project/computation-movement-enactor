94f32d99dc0fe5d680dc0d43cbc05235841e5ce5
Jose Antonio Sanchez <jose.sanchezm@atos.net>
2019-09-26 17:43:27 +0200

    Add missing script

---
+++ jenkins/deploy/deploy-staging.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Staging environment: 31.171.247.162
+# Private key for ssh: /opt/keypairs/ditas-testbed-keypair.pem
+
+# TODO state management? We are killing without caring about any operation the conainer could be doing.
+
+ssh -i /opt/keypairs/ditas-testbed-keypair.pem cloudsigma@31.171.247.162 << 'ENDSSH'
+# Ensure that a previously running instance is stopped (-f stops and removes in a single step)
+# || true - "docker stop" failt with exit status 1 if image doesn't exists, what makes the Pipeline fail. the "|| true" forces the command to exit with 0.
+
+sudo docker stop --time 20 computation-movement-enactor || true
+sudo docker rm --force computation-movement-enactor || true
+sudo docker pull ditas/computation-movement-enactor:latest
+
+sudo docker stop --time 20 mongo || true
+sudo docker rm --force mongo || true
+sudo docker pull mvertes/alpine-mongo:latest
+
+sudo docker run -p 50012:8080 -d --name computation-movement-enactor --link mongo:mongo ditas/computation-movement-enactor:latest
+ENDSSH
